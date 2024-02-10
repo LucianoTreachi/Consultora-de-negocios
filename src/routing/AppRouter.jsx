a87a636e8e7bb9c6708bfc10245c34f9975ea815
lucianotreachi <lucianotreachi@outlook.com>
2024-02-10 12:00:01 -0300

    Modificar estructura de carpetas

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import LayoutRoutes from "./LayoutRoutes";
-import Main from "../page/main/Main";
-import ServicesPage from "../page/servicesPage/ServicesPage";
-import ServiceDetail from "../page/serviceDetail/ServiceDetail";
-import Error404 from "../page/error404/Error404";
+import Main from "../sections/main/Main";
+import ServicesPage from "../pages/servicesPage/ServicesPage";
+import ServiceDetail from "../pages/serviceDetail/ServiceDetail";
+import Error404 from "../pages/error404/Error404";
 
 export default function AppRouter() {
   return (
@@ -12,7 +12,7 @@ export default function AppRouter() {
         <Route path="/" element={<Main />} />
       </Route>
 
-      <Route path="/servicios" element={<ServicesPage />}/>
+      <Route path="/servicios" element={<ServicesPage />} />
 
       <Route path="/servicio/:url" element={<ServiceDetail />} />
 
